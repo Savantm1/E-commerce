00546ccad1610c7b5eaa039f99fc53fdad876487
Savantm1 <42351256+Savantm1@users.noreply.github.com>
2021-03-18 16:08:23 +0300

    поле ввода

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Filters from "./components/Filters/Filters"
+import InputText from "./components/UI/InputText/InputText"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className="body__container">
         <Filters/>
+        <InputText title="hello"/>
       </div>
       <Footer />
     </div>
---
+++ src/components/UI/InputText/InputText.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./InputText.module.scss";
+import mail from  "./../../../assets/icons/ic-contact-mail.svg";
+
+const InputText = (props) => {
+    return (
+        <div className={styles.container}>
+            <label className={styles.title} htmlFor="input">{props.title}</label>
+            <input className={styles.input} id="input" type="text" />
+            <img className={styles.input_img} src={mail} alt="mail"/>
+        </div>
+    )
+}
+
+export default InputText
